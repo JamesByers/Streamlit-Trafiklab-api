68b8e47e5fad5a8ad4a7abccaef06f052f22fb4c
Jim Byers <james.byers.business@gmail.com>
2025-08-25 23:18:06 -0700

    Update Streamlit_resbot_api_test.py

--- code/Streamlit_resbot_api_test.py
+++ code/Streamlit_resbot_api_test.py
@@ -46,7 +46,7 @@ else:
 
 values = get_values_from_nested_dict(data_skanstull)
 
-ct = datetime.now()
+ct = datetime.datetime.now()
 #stockholm_time = ct + relativedelta(hours=1)
 stockholm_time = ct + timedelta(hours=1)
 fmt = "%Y-%m-%d %H:%M"
@@ -365,6 +365,7 @@ if num_trains < 1:
     st.markdown(html_str, unsafe_allow_html=True)
 
     
+
 
 
 
